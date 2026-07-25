e068daa1bac167fa70d8920897bc4d00758edf67
Hirokazu Funaki <44285780+hiro1202@users.noreply.github.com>
2026-07-25 17:47:43 +0900

    abc467-bを追加

---
+++ contests/abc467/b/abc467_b.py
@@ -0,0 +1,12 @@
+# B - Keep the Change
+# https://atcoder.jp/contests/abc467/tasks/abc467_b
+
+
+def main() -> None:
+    """..."""
+    # TODO: 実装する
+    ...
+
+
+if __name__ == "__main__":
+    main()
