90e6ca67df8e64047b4459aff3dd6b9a462a2683
Hirokazu Funaki <44285780+hiro1202@users.noreply.github.com>
2026-07-25 17:40:16 +0900

    abc467aを回答

--- contests/abc467/a/abc467_a.py
+++ contests/abc467/a/abc467_a.py
@@ -4,8 +4,12 @@
 
 def main() -> None:
     """..."""
-    h, w = map(int, input().split())
+    H, W = map(int, input().split())
 
+    # BMI = W / (H/100)^2 >= 25 を判定したいが、H/100 は float になり誤差が出るため下記で判定
+    ok = W * 10000 >= 25 * H * H
+
+    print("Yes" if ok else "No")
 
 if __name__ == "__main__":
     main()
