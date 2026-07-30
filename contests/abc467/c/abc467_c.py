c9bfb52d3709e5e91dd3209257f107be291f281b
Hirokazu Funaki <44285780+hiro1202@users.noreply.github.com>
2026-07-31 00:42:43 +0900

    解き方のメモをコメントで記載

--- contests/abc467/c/abc467_c.py
+++ contests/abc467/c/abc467_c.py
@@ -4,9 +4,16 @@
 
 def main() -> None:
     """..."""
-    # TODO: 実装する
-    ...
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
 
+    candidates = []
+
+    # 操作回数 = 0 で初期化
+    # (A[i] + A[i + 1]) % M = B[i] の判定をする
+    # False なら操作回数に 1 を足す（A[i] に 1 を足す操作が必要になるため）
+    # これを最後まで回して、操作回数を出力する
 
 if __name__ == "__main__":
     main()
