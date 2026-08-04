09cbe9e7e529c70ab6b1808fac44eb8aa052e82a
Hirokazu Funaki <44285780+hiro1202@users.noreply.github.com>
2026-08-05 01:28:11 +0900

    解答を改善

--- contests/abc465/b/abc465_b.py
+++ contests/abc465/b/abc465_b.py
@@ -4,15 +4,11 @@
 
 X, Y, L, R, A, B = map(int, input().split())
 
-a_b = []
-
-for t in range(A + 1, B + 1):
-    a_b.append(t)
-
 cost = 0
 
-for t in a_b:
-    if t >= L + 1 and t <= R:
+# 経過時間で計算するため A + 1 時から B 時までで計算する
+for t in range(A + 1, B + 1):
+    if L + 1 <= t <= R:
         cost += X
     else:
         cost += Y
