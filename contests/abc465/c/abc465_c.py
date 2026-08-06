133de5203470981b5018c173804231b7b50b3006
Hirokazu Funaki <44285780+hiro1202@users.noreply.github.com>
2026-08-07 01:53:27 +0900

    465c 解き途中

--- contests/abc465/c/abc465_c.py
+++ contests/abc465/c/abc465_c.py
@@ -1,3 +1,16 @@
 # C - Reverse Permutation
 # URL: https://atcoder.jp/contests/abc465/tasks/abc465_c
 # 実行: python3 contests/abc465/c/abc465_c.py < contests/abc465/c/tests/sample_1.txt
+
+N = int(input())
+S = input()
+
+A = []
+
+for n in range(1, N + 1):
+    A.append(n)
+
+for s in S:
+    if s == "o":
+        # 反転する
+        print(A)
