90220cb6b571d73a3066e5f3ba76c08d103a68cf
Hirokazu Funaki <44285780+hiro1202@users.noreply.github.com>
2026-08-18 01:06:47 +0900

    468bを解答

--- contests/abc468/b/abc468_b.py
+++ contests/abc468/b/abc468_b.py
@@ -1,3 +1,28 @@
 # B - Corridor Watch
 # URL: https://atcoder.jp/contests/abc468/tasks/abc468_b
 # 実行: python3 contests/abc468/b/abc468_b.py < contests/abc468/b/tests/sample_1.txt
+
+M, D = map(int, input().split())
+S = input()
+
+ans = 0
+
+for i, s in enumerate(S):
+    if s == "G":
+        continue
+
+    flg = True
+
+    # ±D の範囲内に G があれば False
+    for j in range(-D, D + 1):
+        # インデックスエラーを回避
+        if i + j < 0 or i + j >= M:
+            continue
+
+        if S[i + j] == "G":
+            flg = False
+
+    if flg == True:
+        ans += 1
+
+print(ans)
